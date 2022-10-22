77e1716aa94e2f245d134f5ff6bd07453a7613e5
GabrielSilvaSouza <gabrieldasilvasouza21@gmail.com>
2022-10-22 16:08:55 -0300

    builder almost ready

--- dj_copy.py
+++ dj_copy.py
@@ -1,3 +1,4 @@
+import queue
 import math
 
 class NodeLinked(object):
@@ -16,13 +17,13 @@ class Linked(object):
         f = self.head
 
         while (f.sucessor): #find a free slot 
+
             f = f.sucessor
+            
         f.sucessor = p
 
     def printl(self):
-        
         k = self.head
-
         if k is None:
             pass
         else:
@@ -38,13 +39,13 @@ class Linked(object):
             return s
         
         pivot_prev = s
+        print((pivot_prev.vertex))
         curr = s
-        pivot = e.vertex
+        pivot = e.vertex[0]
         #print(e.vertex)
 
-        while (s != e ):
-
-            if (s.vertex[0] < e.vertex[0]):
+        while (s != e):
+            if (s.vertex[0] < pivot):
  
                 # keep tracks of last modified item
                 pivot_prev = curr
@@ -52,31 +53,41 @@ class Linked(object):
                 curr.vertex = s.vertex
                 s.vertex = temp
                 curr = curr.sucessor
-
             s = s.sucessor
  
         temp = curr.vertex
-        curr.vertex = pivot
+        curr.sucessor = pivot
         e.vertex = temp
         
         return pivot_prev
 
 
     def sort(self, s, e):
-
-        if(s == None or s.vertex[0] == e.vertex[0] or s==e.sucessor):#change this last pat of the line
+        
+        
+        if(s == None  or s.vertex[0] == e.vertex[0]):
             return
 
         pivot_prev = self.paritionLast(s, e)
-        self.sort(s, pivot_prev)
         
-      
-        if(pivot_prev != None and pivot_prev.vertex[0] == s.vertex[0]):
+        self.sort(s, pivot_prev)
+
+        if(pivot_prev != None and pivot_prev == s):
+            
+            
             self.sort(pivot_prev.sucessor, e)
 
         elif (pivot_prev != None and pivot_prev.sucessor != None):
             self.sort(pivot_prev.sucessor.sucessor, e)
     
+def runner(node):
+    f = node.head
+    while (f.sucessor  != None): #find a free slot 
+        f = f.sucessor
+    
+    return f
+
+
 def graphBuilderAdjacencyList():
 
     with open(r'grafo_1.txt') as f:
@@ -84,6 +95,7 @@ def graphBuilderAdjacencyList():
         graph = [Linked() for i in range(1,numberVertex+2)]
 
         for line in f:
+
             vertex_a, vertex_b, weight = line.split()
             vertex_a, vertex_b, weight = int(vertex_a), int(vertex_b), int(weight)
             new_node_1 = NodeLinked(vertex_a)
@@ -92,46 +104,48 @@ def graphBuilderAdjacencyList():
             if graph[new_node_1.vertex].head is None:
                 graph[new_node_1.vertex].head = new_node_1
                 graph[new_node_1.vertex].go((weight, vertex_b)) 
+
+                #k = runner(graph[new_node_1.vertex])
+                #print(graph[new_node_1.vertex].head.sucessor)
+                #graph[new_node_1.vertex].sort(graph[new_node_1.vertex].head.sucessor, k)
                 
             else:
+                #print('thought')
                 graph[new_node_1.vertex].go((weight, vertex_b))
 
+                #k = runner(graph[new_node_1.vertex])
+                #print(graph[new_node_1.vertex].head.sucessor)
+                #graph[new_node_1.vertex].sort(graph[new_node_1.vertex].head.sucessor, k)
+                    
+            
             if graph[new_node_2.vertex].head is None:
+                #print('thought')
                 graph[new_node_2.vertex].head = new_node_2
                 graph[new_node_2.vertex].go((weight, vertex_a))
 
+                #k = runner(graph[new_node_2.vertex])
+                #print(graph[new_node_2.vertex].head.sucessor)
+                #graph[new_node_2.vertex].sort(graph[new_node_2.vertex].head.sucessor, k)
+                
+               
             else:
+               
                 graph[new_node_2.vertex].go((weight, vertex_a))
+
+                #k = runner(graph[new_node_2.vertex])
+                #print(graph[new_node_2.vertex].head.sucessor)
+                #graph[new_node_2.vertex].sort(graph[new_node_2.vertex].head.sucessor, k)
+                
+
+    for i in range(0, len(graph)):
+                graph[i].printl()
 
     return graph               
 
-def Dijkstra(graph, root):
-
-    s = root
-    dist = [math.inf]*len(graph)
-    S = []
-    dist[s] = 0
-
-    temp = graph[root].head
-    
-
-    while len(S) != len(graph):
-
-        while graph[root].sucessor:
-            pass
 
 if __name__ == "__main__":
 
     u = graphBuilderAdjacencyList()
-
-    for i in range(1, len(u)):
-        N = u[i].head
-        while (N.sucessor != None):
-            N = N.sucessor
-            
-        u[i].sort(u[i].head.sucessor, N)
-
-    
-    
+    print(u[1].head.sucessor.vertex[0])
 
 
